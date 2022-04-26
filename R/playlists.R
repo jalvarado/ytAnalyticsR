7ece403e37e74cfaccd335831e4492a651dce0e1
Juan Alvarado <jalvarado@ctacorp.com>
2022-04-26 17:04:08 -0400

    normalize function argument naming style across all playlist functions

--- R/playlists.R
+++ R/playlists.R
@@ -1,36 +1,54 @@
-#' Query the analytics API for playlist metrics
+#' Query the analytics API for playlist metrics.
+#'
+#' The playlist ID will be added as a dimension to the returned data.frame.
+#'
+#' @param playlist_id YouTube Playlist ID string
+#' @param start_date Beginning date of the query date range.
+#' @param end_date End date of the query date range.
+#' @param metrics Comma seperated string of requested metrics.
+#' @param dimensions Comma seperated string of requested dimensions.
+#' @param sort Comma seperated string of dimensions to sort by.  Prepend
+#'  dimension name with '-' to indicate a descending sort.
+#' @param filters semi-colon seperated string of filter conditions to apply
+#'  to the result set.
+#' @param ids ID string used to determine which channel or content owner to
+#'  query for data
+#'
+#' @return data.frame containing the API response rows.
 #'
 #' @export
-playlists.query <- function(playlist_id, startDate = NULL, endDate = NULL, metrics = NULL,
-                            dimensions = NULL, sort = NULL, filters = NULL, ids = c("channel==MINE")) {
-  # When querying the API for playlist metrics, the filters must include
-  # 'isCurated==1'
-  curated_filter <- c('isCurated==1')
-  playlist_filter <- paste0('playlist==', playlist_id)
+playlists.query <- function(playlist_id, start_date = NULL,
+                            end_date = NULL, metrics = NULL,
+                            dimensions = NULL, sort = NULL,
+                            filters = NULL, ids = c("channel==MINE")) {
+  curated_filter <- c("isCurated==1")
+  playlist_filter <- paste0("playlist==", playlist_id)
 
   # Check if the isCurated filter has already been passed through the filters
   # argument before adding it.
-  if(is.null(filters) || grepl(curated_filter, filters, fixed=TRUE)) {
-    all_filters <- paste(curated_filter, filters, sep=";")
+  if (is.null(filters) || grepl(curated_filter, filters, fixed = TRUE)) {
+    all_filters <- paste(curated_filter, filters, sep = ";")
   } else {
     all_filters <- filters
   }
 
-  # Add the playlist id to the filter so we only get the metrics for the specified
-  # playlist.
-  all_filters < paste(all_filters, playlist_filter, sep=";")
+  # Add the playlist id to the filter so we only get the metrics for the
+  # specified playlist.
+  all_filters < paste(all_filters, playlist_filter, sep = ";")
 
-  api_args <- list(metrics = metrics, filters = all_filters, startDate = startDate,
-                  endDate = endDate, dimensions = dimensions, sort = sort, ids = ids)
+  api_args <- list(
+    metrics = metrics, filters = all_filters, startDate = start_date,
+    endDate = end_date, dimensions = dimensions, sort = sort, ids = ids
+  )
   r <- do.call(reports.query, rmNullObs(api_args))
 
-  if(length(r$rows) == 0) {
+  if (length(r$rows) == 0) {
     df <- data.frame(matrix(ncol = length(r$columnHeaders$name) + 1, nrow = 0))
-    colnames(df) <- c(r$columnHeaders$name, 'playlist_id')
+    colnames(df) <- c(r$columnHeaders$name, "playlist_id")
   } else {
     df <- as.data.frame(r$rows)
     colnames(df) <- c(r$columnHeaders$name)
-    df['playlist_id'] <- playlist_id
+    df["playlist_id"] <- playlist_id
   }
 
   df
@@ -38,14 +56,26 @@ playlists.query <- function(playlist_id, startDate = NULL, endDate = NULL, metri
 
 #' Take a vector of playlist ids and call playlists.query for each.
 #'
-#' @param playlist_ids Vector of playlist id strings
+#' @param playlist_ids vector of YouTube Playlist ID strings
+#' @param start_date Beginning date of the query date range.
+#' @param end_date End date of the query date range.
+#' @param metrics Comma seperated string of requested metrics.
+#' @param dimensions Comma seperated string of requested dimensions.
+#' @param sort Comma seperated string of dimensions to sort by.  Prepend
+#'  dimension name with '-' to indicate a descending sort.
+#' @param filters semi-colon seperated string of filter conditions to apply
+#'  to the result set.
+#' @param ids ID string used to determine which channel or content owner to
+#'  query for data
+#'
+#' @return data.frame containing the API response rows.
 #'
 #' @return data.frame containing the results from all queries
 #'
 #' @export
 vplaylist.query <- function(playlist_ids, ...) {
   dfs <- list()
-  for(i in 1:length(playlist_ids)) {
+  for (i in seq_length(playlist_ids)) {
     id <- playlist_ids[i]
     dfs[[i]] <- playlists.query(id, ...)
   }
@@ -56,27 +86,34 @@ vplaylist.query <- function(playlist_ids, ...) {
 
 #' Query the Analytics API for playlist demographics metrics
 #'
+#' @param playlist_id YouTube Playlist ID string
+#' @param start_date Beginning date of the query date range.
+#' @param end_date End date of the query date range.
+#'
+#' @return DataFrame
+#'
 #' @export
-playlist.demographics <- function(playlist_id, start_date = NULL, end_date = NULL) {
-  api_args = list(
+playlist.demographics <- function(playlist_id, start_date = NULL,
+                                  end_date = NULL) {
+  api_args <- list(
     startDate = start_date,
     endDate = end_date,
-    ids = 'channel==MINE',
-    filters = paste0('isCurated==1;playlist==', playlist_id),
-    metric = 'viewerPercentage',
-    dimensions = 'gender,ageGroup',
-    sort = 'gender,ageGroup'
+    ids = "channel==MINE",
+    filters = paste0("isCurated==1;playlist==", playlist_id),
+    metric = "viewerPercentage",
+    dimensions = "gender,ageGroup",
+    sort = "gender,ageGroup"
   )
 
   r <- do.call(reports.query, rmNullObs(api_args))
 
-  if(length(r$rows) == 0) {
+  if (length(r$rows) == 0) {
     df <- data.frame(matrix(ncol = length(r$columnHeaders$name) + 1, nrow = 0))
-    colnames(df) <- c(r$columnHeaders$name, 'playlist_id')
+    colnames(df) <- c(r$columnHeaders$name, "playlist_id")
   } else {
     df <- as.data.frame(r$rows)
     colnames(df) <- c(r$columnHeaders$name)
-    df['playlist_id'] <- playlist_id
+    df["playlist_id"] <- playlist_id
   }
 
   df
--- tests/testthat/test_playlists.R
+++ tests/testthat/test_playlists.R
@@ -2,7 +2,7 @@ library(testthat)
 library(ytAnalyticsR)
 
 testthat::test_that("Response rows are converted to dataframe rows", {
-  response_str = '{
+  response_str <- '{
     "kind": "youtubeAnalytics#resultTable",
     "columnHeaders": [
       {
@@ -22,14 +22,14 @@ testthat::test_that("Response rows are converted to dataframe rows", {
     ]
   }'
   api_response <- jsonlite::fromJSON(response_str)
-  mockery::stub(playlists.query, 'reports.query', api_response)
+  mockery::stub(playlists.query, "reports.query", api_response)
 
   playlist_args <- list(
-    startDate = '2022-01-01',
-    endDate = '2022-04-30',
-    metrics = 'views,playlistStarts,viewsPerPlaylistStart,averageTimeInPlaylist',
-    dimensions = 'day',
-    sort = 'day'
+    start_date = "2022-01-01",
+    end_date = "2022-04-30",
+    metrics = "views,playlistStarts,viewsPerPlaylistStart,averageTimeInPlaylist",
+    dimensions = "day",
+    sort = "day"
   )
   playlist_id <- "fake_playlist_id"
   playlist_metrics <- do.call(playlists.query, c(playlist_id, playlist_args))
@@ -39,7 +39,7 @@ testthat::test_that("Response rows are converted to dataframe rows", {
 
 testthat::test_that("Response column headers are converted to data.frame column names", {
   # Empty API response
-  response_str = '{
+  response_str <- '{
     "kind": "youtubeAnalytics#resultTable",
     "columnHeaders": [
       {
@@ -51,14 +51,14 @@ testthat::test_that("Response column headers are converted to data.frame column 
     "rows": []
   }'
   api_response <- jsonlite::fromJSON(response_str)
-  mockery::stub(playlists.query, 'reports.query', api_response)
+  mockery::stub(playlists.query, "reports.query", api_response)
 
   playlist_args <- list(
-    startDate = '2022-01-01',
-    endDate = '2022-04-30',
-    metrics = 'views,playlistStarts,viewsPerPlaylistStart,averageTimeInPlaylist',
-    dimensions = 'day',
-    sort = 'day'
+    start_date = "2022-01-01",
+    end_date = "2022-04-30",
+    metrics = "views,playlistStarts,viewsPerPlaylistStart,averageTimeInPlaylist",
+    dimensions = "day",
+    sort = "day"
   )
   playlist_id <- "fake_playlist_id"
   playlist_metrics <- do.call(playlists.query, c(playlist_id, playlist_args))
@@ -69,7 +69,7 @@ testthat::test_that("Response column headers are converted to data.frame column 
 
 test_that("An empty API response results in an empty data.frame", {
   # Empty API response
-  response_str = '{
+  response_str <- '{
     "kind": "youtubeAnalytics#resultTable",
     "columnHeaders": [
       {
@@ -81,18 +81,17 @@ test_that("An empty API response results in an empty data.frame", {
     "rows": []
   }'
   api_response <- jsonlite::fromJSON(response_str)
-  mockery::stub(playlists.query, 'reports.query', api_response)
+  mockery::stub(playlists.query, "reports.query", api_response)
 
   playlist_args <- list(
-    startDate = '2022-01-01',
-    endDate = '2022-04-30',
-    metrics = 'views,playlistStarts,viewsPerPlaylistStart,averageTimeInPlaylist',
-    dimensions = 'day',
-    sort = 'day'
+    start_date = "2022-01-01",
+    end_date = "2022-04-30",
+    metrics = "views,playlistStarts,viewsPerPlaylistStart,averageTimeInPlaylist",
+    dimensions = "day",
+    sort = "day"
   )
   playlist_id <- "fake_playlist_id"
   playlist_metrics <- do.call(playlists.query, c(playlist_id, playlist_args))
 
   testthat::expect_equal(nrow(playlist_metrics), 0)
 })
-
