380a9e309f7f7205bdec598d0847e06e66b83c1c
Juan Alvarado <wok1@cdc.gov>
2022-06-10 09:00:37 -0400

    Calling yt_analytics_auth will also setup tubeR auth.
    Fetching or refreshing a token using the yt_analytics_auth method will
    also set the option "google_token" which is used by tubeR to check if
    the user already has a valid OAuth 2.0 token.

--- R/auth.R
+++ R/auth.R
@@ -1,20 +1,45 @@
 .auth <- NULL
 
+#   "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
+#   "https://www.googleapis.com/auth/youtubepartner",
+#   "https://www.googleapis.com/auth/youtube",
 default_scopes <- c(
-  "https://www.googleapis.com/auth/youtube",
   "https://www.googleapis.com/auth/youtube.readonly",
-  "https://www.googleapis.com/auth/youtubepartner",
-  "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
   "https://www.googleapis.com/auth/yt-analytics.readonly"
 )
 
+
 #' Authorize YouTube Analytics API
+#'
+#' Authorize 'ytAnalyticsR' to query the YouTube Analytics and Reporting APIs
+#'
+#' By default you are directred to a web browser, asked to sign in to your Google
+#' account, and to grant ytAnalyticsR permission to operate on your behalf with
+#' YouTube Analytics and Reporting.
+#'
+#' @param email Optional. Allows user to target a specific Google identity. Defaults to the option named "gargle_oauth_email", retrieved by gargle_oauth_email().
+#' @param scopes A character vector of scopes to request.
+#' @param cache Specified the OAuth token cache. Defaults to the option named "gargle_oauth_cache", retrieved via gargle_oauth_cache().
+#' @param use_oob Whether to prefer "out of band" authentication. Defaults to the option named "gargle_oob_default", retrieved via gargle_oob_default().
+#' @param token A token with class Token2.0 or an object of httr's class request, i.e. a token that has been prepared with httr::config() and has a Token2.0 in the auth_token component.
 #'
 #' @family auth
 #' @export
 #'
+#' @examples
+#' if (FALSE) {
+#'   # Load/refresh existing credentials if available
+#'   # otherwise go through the OAuth 2.0 flow for authorization
+#'   # and authentication.
+#'   yt_analytics_auth()
+#'
+#'   # Force use of a token specified by the given email
+#'   yt_analytics_auth(email = "user@example.com")
+#'
+#'   # Force the OAuth web flow
+#'   yt_analytics_auth(email = NA)
+#' }
 yt_analytics_auth <- function(email = gargle::gargle_oauth_email(),
-                              path = NULL,
                               scopes = default_scopes,
                               cache = gargle::gargle_oauth_cache(),
                               use_oob = gargle::gargle_oob_default(),
@@ -23,7 +48,6 @@ yt_analytics_auth <- function(email = gargle::gargle_oauth_email(),
     scopes = scopes,
     app = yt_analytics_oauth_app(),
     email = email,
-    path = path,
     package = "ytanalyticsr",
     cache = cache,
     use_oob = use_oob,
@@ -37,10 +61,13 @@ yt_analytics_auth <- function(email = gargle::gargle_oauth_email(),
   .auth$set_cred(cred)
   .auth$set_auth_active(TRUE)
 
+  # use the auth token we just fetched to auth with tubeR as well
+  options(google_token = .auth$cred)
+
   invisible()
 }
 
-#' remove any existing credentials for the current session.
+#' Remove any existing credentials for the current session.
 #'
 #' @export
 yt_analytics_deauth <- function() {
