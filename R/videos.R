2c801ae9f6704c4be65967e40c47a9ac9f68cd50
Juan Alvarado <wok1@cdc.gov>
2022-07-14 17:08:37 -0400

    Fix bug that was causing video.query to always return an empty data.frame

--- R/videos.R
+++ R/videos.R
@@ -16,31 +16,29 @@
 video.query <- function(video_id, metrics = NULL, filters = NULL,
                         start_date = NULL, end_date = NULL, dimensions = NULL,
                         sort = NULL, ids = c("channel==MINE")) {
-  video_filter <- paste0("video==", video_id)
-
-  if (is.null(filters)) {
-    all_filters <- video_filter
-  } else {
-    all_filters <- paste(filters, video_filter, sep = ";")
-  }
-
-  api_args <- list(
-    metrics = metrics, filters = all_filters,
-    startDate = start_date, endDate = end_date,
-    dimensions = dimensions, sort = sort, ids = ids
+  query_filters <- paste(
+    paste0("video==", video_id),
+    filters,
+    sep = ";"
   )
-  r <- do.call(reports.query, rmNullObs(api_args))
 
-  if (length(r$rows) == 0) {
-    df <- data.frame(matrix(ncol = length(r$columnHeaders$name) + 1, nrow = 0))
-    colnames(df) <- c(r$columnHeaders$name, "video_id")
+  data <- reports.query(
+    startDate = start_date,
+    endDate = end_date,
+    metrics = metrics,
+    dimensions = dimensions,
+    filters = query_filters,
+    ids = ids
+  )
+
+  if (nrow(data) == 0) {
+    df <- data.frame(matrix(ncol = length(names(data)) + 1, nrow = 0))
+    colnames(df) <- c(names(data), "video_id")
   } else {
-    df <- as.data.frame(r$rows)
-    colnames(df) <- c(r$columnHeaders$name)
-    df["video_id"] <- video_id
+    df <- data
+    df$video_id <- video_id
   }
-
-  df
+  return(df)
 }
 
 #' Query the Analytics API and retrieve demographics information
--- tests/testthat/test_videos.R
+++ tests/testthat/test_videos.R
@@ -1,30 +1,13 @@
 testthat::test_that("video.query calls reports.query", {
-  response_str <- '{
-    "kind": "youtubeAnalytics#resultTable",
-    "columnHeaders": [
-      {
-        "name": "views",
-        "dataType": "STRING",
-        "columnType": "METRIC"
-      },
-      {
-        "name": "day",
-        "dataType": "STRING",
-        "columnType": "DIMENSION"
-      }
-    ],
-    "rows": [
-      ["foo", "bar"],
-      ["row2", "fake_data"]
-    ]
-  }'
-  mock_reports_query <- mockery::mock(jsonlite::fromJSON(response_str))
+  mock_data <- data.frame(views = c("foo", "row2"),
+                          day = c("2022-01-01", "2022-01-02"))
+  mock_reports_query <- mockery::mock(mock_data)
   mockery::stub(video.query, "reports.query", mock_reports_query)
 
   video_id <- "fake_video_id123"
   video_metrics <- video.query(video_id,
     start_date = "2022-01-01",
-    end_date = "2022-04-30",
+    end_date = "2022-01-02",
     metrics = "views",
     dimensions = "day",
     sort = "day"
@@ -35,29 +18,14 @@ testthat::test_that("video.query calls reports.query", {
 })
 
 testthat::test_that("an empty api response returns an empty dataframe", {
-  response_str <- '{
-    "kind": "youtubeAnalytics#resultTable",
-    "columnHeaders": [
-      {
-        "name": "views",
-        "dataType": "STRING",
-        "columnType": "METRIC"
-      },
-      {
-        "name": "day",
-        "dataType": "STRING",
-        "columnType": "DIMENSION"
-      }
-    ],
-    "rows": []
-  }'
-  mock_reports_query <- mockery::mock(jsonlite::fromJSON(response_str))
+  mock_data <- data.frame(views = c(), day = c())
+  mock_reports_query <- mockery::mock(mock_data)
   mockery::stub(video.query, "reports.query", mock_reports_query)
 
   video_id <- "fake_video_id123"
   video_metrics <- video.query(video_id,
     start_date = "2022-01-01",
-    end_date = "2022-04-30",
+    end_date = "2022-01-02",
     metrics = "views",
     dimensions = "day",
     sort = "day"
@@ -68,71 +36,14 @@ testthat::test_that("an empty api response returns an empty dataframe", {
   testthat::expect_s3_class(video_metrics, "data.frame")
 })
 
-testthat::test_that("api response rows are converted to dataframe rows", {
-  response_str <- '{
-    "kind": "youtubeAnalytics#resultTable",
-    "columnHeaders": [
-      {
-        "name": "views",
-        "dataType": "STRING",
-        "columnType": "METRIC"
-      },
-      {
-        "name": "day",
-        "dataType": "STRING",
-        "columnType": "DIMENSION"
-      }
-    ],
-    "rows": [
-      ["value1", "value2"],
-      ["value3", "value4"]
-    ]
-  }'
-  mock_reports_query <- mockery::mock(jsonlite::fromJSON(response_str))
-  mockery::stub(video.query, "reports.query", mock_reports_query)
-
-  video_args <- list(
-    start_date = "2022-01-01",
-    end_date = "2022-04-30",
-    metrics = "views",
-    dimensions = "day",
-    sort = "day"
-  )
-  video_id <- "fake_video_id123"
-  video_metrics <- do.call(video.query, c(video_id, video_args))
-
-  mockery::expect_called(mock_reports_query, 1)
-  testthat::expect_equal(nrow(video_metrics), 2)
-  testthat::expect_equal(video_metrics[[1]], c("value1", "value3"))
-  testthat::expect_s3_class(video_metrics, "data.frame")
-})
-
 testthat::test_that("the video_id column is added to the data.frame", {
-  response_str <- '{
-    "kind": "youtubeAnalytics#resultTable",
-    "columnHeaders": [
-      {
-        "name": "views",
-        "dataType": "STRING",
-        "columnType": "METRIC"
-      },
-      {
-        "name": "day",
-        "dataType": "STRING",
-        "columnType": "DIMENSION"
-      }
-    ],
-    "rows": [
-      ["value1", "value2"],
-      ["value3", "value4"]
-    ]
-  }'
-  mock_reports_query <- mockery::mock(jsonlite::fromJSON(response_str))
+  mock_data <- data.frame(views = c(1, 0), day = c("2022-01-01", "2022-01-02"))
+  mock_reports_query <- mockery::mock(mock_data)
   mockery::stub(video.query, "reports.query", mock_reports_query)
 
   video_args <- list(
     start_date = "2022-01-01",
-    end_date = "2022-04-30",
+    end_date = "2022-01-02",
     metrics = "views",
     dimensions = "day",
     sort = "day"
@@ -145,62 +56,46 @@ testthat::test_that("the video_id column is added to the data.frame", {
   testthat::expect_equal(video_metrics$video_id, c(video_id, video_id))
 })
 
-testthat::test_that("vvideo.query makes multiple calls to the api", {
-  response_str1 <- '{
-    "kind": "youtubeAnalytics#resultTable",
-    "columnHeaders": [
-      {
-        "name": "views",
-        "dataType": "STRING",
-        "columnType": "METRIC"
-      },
-      {
-        "name": "day",
-        "dataType": "STRING",
-        "columnType": "DIMENSION"
-      }
-    ],
-    "rows": [
-      ["value1", "value2"]
-    ]
-  }'
-  response_str2 <- '{
-    "kind": "youtubeAnalytics#resultTable",
-    "columnHeaders": [
-      {
-        "name": "views",
-        "dataType": "STRING",
-        "columnType": "METRIC"
-      },
-      {
-        "name": "day",
-        "dataType": "STRING",
-        "columnType": "DIMENSION"
-      }
-    ],
-    "rows": [
-      ["value3", "value4"]
-    ]
-  }'
+test_that("the video_id column is added to an empty dataframe response", {
+  mock_data <- data.frame(views = NULL, day = NULL)
+  expect_equal(nrow(mock_data), 0)
+  mock_reports_query <- mockery::mock(mock_data)
+  mockery::stub(video.query, "reports.query", mock_reports_query)
 
-  r1 <- jsonlite::fromJSON(response_str1)
-  r2 <- jsonlite::fromJSON(response_str2)
-  mock_responses <- mockery::mock(r1, r2)
-  mockery::stub(vvideo.query, "reports.query", mock_responses, depth = 2)
+  video_args <- list(
+    start_date = "2022-01-01",
+    end_date = "2022-01-02",
+    metrics = "views",
+    dimensions = "day",
+    sort = "day"
+  )
+  video_id <- "fake_video_id123"
+  video_metrics <- do.call(video.query, c(video_id, video_args))
+
+  expect_true("video_id" %in% names(video_metrics))
+  expect_equal(nrow(video_metrics), 0)
+})
+
+testthat::test_that("vvideo.query makes multiple calls to video.query", {
+  video1 <- data.frame(views = c(1), day = c("2022-01-01"), video_id = c("fake_video_id123"))
+  video2 <- data.frame(views = c(2), day = c("2022-01-01"), video_id = c("totally_not_fake"))
+  mock <- mockery::mock(video1, video2)
+  mockery::stub(vvideo.query, "video.query", mock)
 
   video_ids <- c("fake_video_id123", "totally_not_fake")
   video_metrics <- vvideo.query(video_ids,
     start_date = "2022-01-01",
-    end_date = "2022-04-30",
+    end_date = "2022-01-01",
     metrics = "views",
     dimensions = "day",
     sort = "day"
   )
 
-  mockery::expect_called(mock_responses, 2)
+  mockery::expect_called(mock, 2)
   testthat::expect_equal(nrow(video_metrics), 2)
   testthat::expect_s3_class(video_metrics, "data.frame")
-  testthat::expect_equal(video_metrics$views, c("value1", "value3"))
+  testthat::expect_equal(video_metrics$views, c(1, 2))
+  testthat::expect_equal(video_metrics$video_id, video_ids)
 })
 
 testthat::test_that("vvideo.demographics calls vvideo.query for each id", {
